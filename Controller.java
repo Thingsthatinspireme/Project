bdd98fa2a909152fad9cdd548793ded3bf5369df
Thingsthatinspireme <73827546+Thingsthatinspireme@users.noreply.github.com>
2020-12-25 02:15:03 +0900

    저기요 5일차

--- Controller.java
+++ Controller.java
@@ -1,33 +1,225 @@
 package ex01.program.Jeokiyo;
 
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class Controller {
 
-	public void restaurantInfo() {
+	DAO dao = new DAO();
+	VO vo  = new VO();
+	static int OrderNum = 0;
 
+	
+	VO[] restList(String Category, VO[] infoCate) {
+		List list = dao.restList(Category);
+		for (int i = 0; i < list.size(); i++) {
+			infoCate[i] = (VO) list.get(i);
+		}
+		return infoCate;
+	}//category를통한 식당 정보 불러오기	
+		
+	VO[] restInfo(String RestName, VO[] infoRest) {
+		List list = dao.restInfo(RestName);
+		for (int i = 0; i < list.size(); i++) {
+			infoRest[i] = (VO) list.get(i);
+			
+		}
+		return infoRest;
+	}//restInfo를 통한 해당 식당의 총 정보 불러오기
+
+
+	VO[] logUserInfo (String UserId, VO[] infoUser) {
+		List list = dao.logUserInfo(UserId);
+		for (int i = 0; i < list.size(); i++) {
+			infoUser[i] = (VO) list.get(i);
+			
+		}
+		return infoUser;
+	}//restInfo를 통한 해당 식당의 총 정보 불러오기	
+	
+	
+	void category() {
+		View.foodCategories();//로그인창에 넣어서 연결시켜줘야 함 
 	}
 
-	public void mainRun() {
-		int choice;
-		View.foodCategories();
-		choice = Model.getSetMenu();
-		switch (choice) {
+	VO[] categoryRun(){
+
+		String Category = null;
+
+		VO[] infoCate = new VO [4];
+				
+		int choice = VO.getSetMenu();
+
+		switch(choice) {
+
 		case 1: 
-			View.korFood(); 
+			Category = "한식"; 
+			break; 
+		case 2:	
+			Category = "중식"; 
+			break; 
+		case 3: 
+			Category = "일식"; 
+			break; 
+		case 4: 
+			Category = "일품"; 
+			break; 
+		default : 
+			System.out.println("잘못 선택하셨습니다.");	
+		}
+
+		
+		infoCate = restList(Category, infoCate);
+
+		View.restaurantInfo(infoCate, choice, Category);
+
+		return infoCate;
+	
+	}	//식당 카테고리 선정 -> 식당 리스트로 들어가기 
+
+	VO[] restInfoRun(VO[] infoCate) {
+	
+		int choice = VO.getSetMenu();
+		VO[] infoRest = new VO[10];
+		String RestName = null;
+
+		switch(choice) {
+		case 0:
+			category(); break;
+		case 1: 
+			RestName = infoCate[choice-1].getRestName(); 
 			break;
 		case 2: 
-			//View.chiFood(); 
+			RestName = infoCate[choice-1].getRestName(); 
 			break;
 		case 3: 
-			 
+			RestName = infoCate[choice-1].getRestName(); 
 			break;
 		case 4: 
-		
+			RestName = infoCate[choice-1].getRestName(); 
 			break;
-		default : System.out.println("잘못선택하셨습니다");
-			
-
+		default : 
+			System.out.println("잘못 선택하셨습니다.");
 		}
+		
+		
+		infoRest = restInfo(RestName, infoRest);
+		String Address = infoRest[0].getRestAddress();
+		String Phone = infoRest[0].getPhone();
+		int MinPrice = infoRest[0].getMinPrice();
+		 
+		View.restaurantMenu(infoRest);
+		
+		return infoRest;
+	}//식당 리스트에서 장바구니로 넘어가기 
+	
+	
+	ArrayList<VO> orderRun(VO[] infoRest) {
+		
+
+		int MinPrice = infoRest[0].getMinPrice();
+		int choice =0;
+		int Total = 0;
+		int cnt = 0;
+		
+
+		ArrayList<VO> basket= new ArrayList<VO>();
+		
+		System.out.println("주문하실 메뉴를 선택해주세요 ☞");	
+		
+		while(true) {
+			choice = VO.getOrder();
+			int FoodCnt = 1;
+			if(choice == 0)
+				break;
+						
+			basket.add(infoRest[choice-1]);
+			Total += basket.get(cnt).getUnitPrice();
+			
+			
+			System.out.println(infoRest[choice-1].getFoodName()+ "가 장바구니에 추가됐습니다.");
+			System.out.println("총 금액 :" + Total);
+	
+			System.out.println("-------------------------------------------");
+			
+			if( Total < infoRest[choice-1].getMinPrice()) {
+				View.orderPopUp();
+			}else{
+				System.out.println("장바구니로가려면 0번을 눌러주세요☞");
+				System.out.println();
+				System.out.println("계속 고르려면 추가 할 메뉴번호를 눌러주세요 ☞");
+				System.out.println("-------------------------------------------");
+			}
+			
+		}
+		
+		View.orderList(basket, Total);
+		return basket;
+	}//식당 메뉴창에서 주문메세지 나오게 한것 
+
+	
+	public void payment(ArrayList<VO> basket, String UserId) throws InterruptedException {
+		VO[] infoUser = new VO[1];
+		int Total = 0;
+		
+		for (int i = 0; i < basket.size(); i++) {
+			Total += basket.get(i).getUnitPrice();
+		}
+		
+		infoUser = logUserInfo(UserId, infoUser);
+		System.out.println("배달 주문하시려면 1번을 눌러주세요☞ ");
+		int choice = VO.getOrder();
+		if(choice == 1) 
+			View.payment(infoUser, basket, Total); //결제창 출력
+				
+		String pay[] = {"현금/카드 ", "휴대폰 결제", "까까오페이"};
+		System.out.println("결제 방식을 입력해주세요 ☞ ");
+		choice = VO.getOrder();
+		
+		boolean boo = true;
+		while(boo) {
+			switch (choice) {
+			case 1 :
+				System.out.println(pay[choice-1] +" 결제를 선택하셨습니다"); 
+				break;
+			case 2 :
+				System.out.println(pay[choice-1] +" 결제를 선택하셨습니다"); 
+				break;
+			case 3 : 
+				System.out.println(pay[choice-1] +" 결제를 선택하셨습니다"); 
+				break;
+			default :
+				System.out.println("결제방식을 선택해주세요");
+			}
+			System.out.println("결제하시려면 4번을 눌러주세요");
+			choice = VO.getOrder();
+			if(choice == 4) {
+				View.payment2(); //결제를 시작합니다 출력
+				Thread.sleep(1500);
+				System.out.println();
+				View.payment3(); //결제중입니다 출력
+			}	
+			boo = false;
+		
+		}		
+	}//결제창
+	
+		
+	public void totalRun() throws InterruptedException {
+		
+		VO[] infoCate = null;
+		VO[] infoRest = null;
+		String UserId = "parkbloger";
+		ArrayList<VO> basket;
+
+			category();
+			infoCate = categoryRun();
+			infoRest = restInfoRun(infoCate); // 식당 메뉴
+			basket= orderRun(infoRest); //주문
+			payment(basket, UserId);
+
 	}
 }
--- DAO.java
+++ DAO.java
@@ -9,66 +9,178 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DAO {
-	//3. 클래스변수 선언
-	Connection con = null;  //오라클 접속에 필요
-	PreparedStatement preStmt = null; //SQL문 실행에 필요
-	ResultSet rs = null; //쿼리문 처리 후 결과값 저장에 필요
-	int cnt = 0;	
+	// 3. 클래스변수 선언
+	Connection con = null; // 오라클 접속에 필요
+	PreparedStatement preStmt = null; // SQL문 실행에 필요
+	ResultSet rs = null; // 쿼리문 처리 후 결과값 저장에 필요
+	int cnt = 0;
 
 	void conn() {
-			//1.오라클 접속에 필요한 클래스를 메모리에 로딩
-			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			//2.url,id,pass 저장
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			String id = "bitcamp";
-			String pass = "bitcamp";
-
-			//4. 오라클 접속
-			try {
-				con = DriverManager.getConnection(url,id,pass);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-	}
-//한식
-	public VO korFood() {
-//		List list = new ArrayList();
-		VO vo = new VO();
+		// 1.오라클 접속에 필요한 클래스를 메모리에 로딩
 		try {
-			conn();
-			//셀렉트문 입력 
-			String userSelect="SELECT * "
-							+ "FROM restinfo "
-							+ "WHERE category = '한식'";
-			preStmt = con.prepareStatement(userSelect);
-			rs = preStmt.executeQuery();
-			while(rs.next()) {
-				String RestName = rs.getString("RestName");
-				String Category  = rs.getString("Category");
-				int Grade = rs.getInt("Grade");			
-				int MiniPrice = rs.getInt("MiniPrice");
-				
-				vo.setRestName(RestName);
-				vo.setCategory(Category);
-				vo.setGrade(Grade);
-				vo.setMiniPrice(MiniPrice);
-				
-//				list.add(vo);
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// 2.url,id,pass 저장
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "bitcamp";
+		String pass = "bitcamp";
 
-			 }
-			rs.close();
-			preStmt.close();
-			con.close();
-			
+		// 4. 오라클 접속
+		try {
+			con = DriverManager.getConnection(url, id, pass);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return vo;
-	}
 
+	}
+	
+
+	public List restList(String category) {
+		List list = new ArrayList();
+
+		try {
+			conn();
+			// 셀렉트문 입력
+			String userSelect = "SELECT rest_name, minprice " +
+								" FROM restaurant " +
+								" WHERE category = ?";
+			
+			preStmt = con.prepareStatement(userSelect);
+			preStmt.setString(1, category);
+			rs = preStmt.executeQuery();
+
+			while (rs.next()) {
+	
+				String RestName = rs.getString("rest_name");
+				int MinPrice = rs.getInt("minprice");
+				VO vo = new VO();
+				vo.setRestName(RestName);
+				vo.setMinPrice(MinPrice);
+
+				list.add(vo);
+			
+			}
+			rs.close();
+			preStmt.close();
+			con.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}//각 카테고리 식당 리스트 select 검색
+	
+	
+	public List restInfo(String Rest) {
+		List list = new ArrayList();
+
+		
+		try {
+			conn();
+			// 셀렉트문 입력
+			String userSelect = "SELECT  r.rest_reginum, r.rest_name, r.rest_ad, r.rest_phonenum, u.foodname, u.unitprice, r.minprice" +
+								" FROM restaurant r, unitrestmenu u" +
+								" WHERE r.rest_reginum = u.rest_reginum" +
+								" AND r.rest_name = ?";
+			
+			preStmt = con.prepareStatement(userSelect);
+			preStmt.setString(1, Rest);
+			rs = preStmt.executeQuery();
+
+			while (rs.next()) {
+	
+				String RestName = rs.getString("rest_name");
+				String RestAddress = rs.getString("rest_ad");
+				String FoodName = rs.getString("foodname");
+				String Phone = rs.getString("rest_phonenum");
+				int MinPrice = rs.getInt("minprice");
+				int UnitPrice = rs.getInt("unitprice");
+				VO vo = new VO();
+				vo.setRestName(RestName);
+				vo.setRestAddress(RestAddress);
+				vo.setFoodName(FoodName);
+				vo.setPhone(Phone);
+				vo.setMinPrice(MinPrice);
+				vo.setUnitPrice(UnitPrice);
+				
+				list.add(vo);
+
+			}
+			rs.close();
+			preStmt.close();
+			con.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}//각 카테고리 식당 메뉴 select 검색
+	
+
+	public List logUserInfo(String UserId) {
+		List list = new ArrayList();
+
+		
+		try {
+			conn();
+			// 셀렉트문 입력
+			String userSelect = "SELECT user_phonenum, user_ad" +
+								" FROM users" +
+								" WHERE user_id = ?";
+			
+			preStmt = con.prepareStatement(userSelect);
+			preStmt.setString(1, UserId);
+			rs = preStmt.executeQuery();
+
+			while (rs.next()) {
+	
+				String UserPhoneNum = rs.getString("user_phonenum");
+				String UserAddress = rs.getString("user_ad");
+			
+				VO vo = new VO();
+	
+				vo.setUserPhoneNum(UserPhoneNum);
+				vo.setUserAddress(UserAddress);
+	
+				
+				list.add(vo);
+
+			}
+			rs.close();
+			preStmt.close();
+			con.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}//로그인한 사람의 정보 검색 
+	
+
+	
+//	public void addOrder(VO vo) {
+//		try {
+//			conn();
+//			String userInsert = "INSERT INTO member(UserName,UserId,UserPw,UserPhoneNum,UserAdd) VALUES(?,?,?,?,?)";
+//			preStmt = con.prepareStatement(userInsert); //pre는 statement에 쿼리문을 넣는다.
+//			//for (int i = 0; i < 100; i++) {
+//				preStmt.setString(1, vo.getUserName());
+//				preStmt.setString(2, vo.getUserId());
+//				preStmt.setInt(3, vo.getUserPw());
+//				preStmt.setInt(4, vo.getUserPhoneNum());
+//				preStmt.setInt(5, vo.getUserAdd());
+////			}
+//			preStmt.executeUpdate(); //insert 와 update는 이걸로 그냥은 여기에 쿼리문을 넣는다.
+//			preStmt.close();
+//			con.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//	} //주문정보 데베로넘기기 
+
+
+	
 }
